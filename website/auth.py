9217d3847f26328b88a82d567349e98b463eb54b
MHP678 <muhammadpatel465@gmail.com>
2023-03-17 10:54:31 +0000

    "SignUp page working"

--- website/auth.py
+++ website/auth.py
@@ -56,7 +56,7 @@ def Sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Registration Successful', category='success')
             return redirect(url_for('views.home'))
 

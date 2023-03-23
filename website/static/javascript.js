bf3783f049b19fc31d7834f4b573ffc9ff5221f1
moneshapb <monesha.phillipsbrown@barclays.com>
2023-03-23 13:06:38 +0000

    drawing app and styling

--- website/static/javascript.js
+++ website/static/javascript.js
@@ -1,153 +1,105 @@
-$(function(){
-    
+$(function() {
+var paint = false;
+
+
+var paint_erase = "paint";
+
+
+var canvas = document.getElementById("paint");
+var ctx = canvas.getContext("2d");
 
     
-    //declare variables
-    
-    //paintingerasing or not
-    var paint = false; 
-    
-    //painting or erasing 
-    var paint_erase = "paint"; 
-    
-    //get the canvas and context
-    var canvas = document.getElementById("paint"); 
-    var ctx = canvas.getContext("2d"); 
-    
-    
-    //get the canvas container
-    var container = $("#container");
-    
-    //mouse position
-    var mouse = {x: 0, y: 0}; 
-    
-    
-    //onload load saved work from localStorage 
-    
-    if(    localStorage.getItem("imgCanvas") != null){
-       var img = new Image(); 
-        img.onload = function(){
-            ctx.drawImage(img, 0, 0);
-            
-        }
-        img.src = localStorage.getItem("imgCanvas");
-        
-    };
-    
-    //set drawing parameters (linewidth,linejoin,linecap)
-    
-    ctx.lineWidth =3;
-    ctx.lineJoin = "round";
-    ctx.lineCap = "round"; 
-    
-    
-    //click inside container
-    
-    container.mousedown(function(e){
-        paint = true;
-        ctx.beginPath();
-        mouse.x = e.pageX - this.offsetLeft;
-        mouse.y = e.pageY - this.offsetTop;
-        ctx.moveTo(mouse.x, mouse.y);
-    });
-    
-    //move the mouse while holding mouse key
-      container.mousemove(function(e){
-      mouse.x = e.pageX - this.offsetLeft;
-      mouse.y = e.pageY - this.offsetTop;
-      if(paint == true){
-         if(paint_erase == "paint"){
-             ctx.strokeStyle = $("#paintColor").val();
-             //get color input
-              }else{
-                  //white color
-              ctx.strokeStyle = "white";    
-              } 
-          ctx.lineTo(mouse.x, mouse.y);
-          ctx.stroke();
-      }
-    });
-    //mouse up-->we are not paintingerasing anymore#
-    
-    container.mouseup(function(){
-        paint = false;
-        
-    });
-    
-    
-    //if we leave the container we are not painting easring anymore 
-      container.mouseleave(function(){
-        paint = false;
-        
-    });
-    
-    
-    
-    
-    //click on the reset button
-    $("#reset").click(function(){
-      ctx.clearRect(0, 0, canvas.width, canvas.height);  
-        paint_erase = "paint";
-        $("#erase").removeClass("eraseMode");
-    });
-    //click on the save button 
-    $("#save").click(function(){
-        if(typeof(localStorage)!=null){
-    
-    localStorage.setItem("imgCanvas", canvas.toDataURL());
-            
-}else{
-    window.alert("Your browser does not support local storage");
+
+ var container = $("#container");
+var mouse = {
+x: 0,  y: 0
+};
+ 
+if (localStorage.getItem("imgCanvas") != null) {
+var img = new Image();
+img.onload = function() {
+ctx.drawImage(img, 0, 0);}
+img.src = localStorage.getItem("imgCanvas");
 }
-        
-        
-    });
-    
-    // click on the erase button
-    
-    $("#erase").click(function(){
-        if(paint_erase == "paint"){
-            paint_erase = "erase";
-        }else{
-            paint_erase = "paint";
-        }
-        
-        $(this).toggleClass("eraseMode");
-    });
-    
-    
-    
-    //change color input
-    $("#paintColor").change(function(){
-       $("#circle").css("background-color", 
-                       
-    $(this).val()); 
-        
-    });
-    
-    //change linewidth using slider
-     $("#slider").slider({
-    min: 3,
-    max: 30,
-   slide: function(event, ui){
-       $("#circle").height(ui.value);
-       $("#circle").width(ui.value);
-       ctx.lineWidth = ui.value;
-   }  
-     
- });   
-    
-    
-    //functions
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ctx.lineWidth = 3;
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+container.mousedown(function(e) {
+paint = true;
+ctx.beginPath();
+mouse.x = e.pageX - this.offsetLeft;
+mouse.y = e.pageY - this.offsetTop;
+ctx.moveTo(mouse.x, mouse.y);
 });
+container.mousemove(function(e) {
+mouse.x = e.pageX - this.offsetLeft;
+mouse.y = e.pageY - this.offsetTop;
+if (paint == true) {
+if (paint_erase == "paint") {
+ctx.strokeStyle = $("#paintColor").val();
+}
+else {
+ctx.strokeStyle = "white";
+}
+ctx.lineTo(mouse.x, mouse.y);
+ctx.stroke();
+}
+});
+container.mouseup(function() {
+paint = false;}
+
+);
+container.mouseleave(function() {
+   paint = false;}
+);
+   
+   $("#erase").click(function() {
+if (paint_erase == "paint") {
+paint_erase = "erase";
+}
+else {
+paint_erase = "paint";
+}
+
+    $(this).toggleClass("eraseMode");
+}
+);
+$("#reset").click(function() {
+ctx.clearRect(0, 0, 500, 300);  //clears the canvas
+$("#erase").removeClass("eraseMode"); //removes the erase mode class
+paint_erase = "paint"; //sets the paint mode
+$("#paintColor").val("#000000"); //sets the color to black
+$("#slider").slider("value", 3); //sets the slider to 3
+ctx.lineWidth = 3; //sets the line width to 3
+}
+);
+$("#paintColor").change(function() {
+$("#circle").css("background-color", $(this).val());
+}
+);
+$("#slider").slider({
+min: 3,
+max: 30,
+slide: function(event, ui) {
+$("#circle").height(ui.value);
+$("#circle").width(ui.value);
+ctx.lineWidth = ui.value;
+}
+}
+);
+$("#save").click(function() {
+if (typeof(localStorage) != null) {
+localStorage.setItem("imgCanvas", canvas.toDataURL());
+}
+else {
+window.alert("Your browser does not support local storage!");
+}
+}
+);
+}
+);
+
+// Path: website\static\javascript.js
+// Compare this snippet from javascript.js:
+//     ctx.lineCap = "round";
+//
--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
+
 # Standard routes for the app , where users can actually go to
+
 # url deifned here , seperate app from blueprint
 import datetime
 from flask import Blueprint, render_template , flash , request , jsonify ,  redirect, url_for
@@ -6,21 +8,18 @@ from flask_login import  login_required, current_user
 from .models import Note , Todo
 from . import db
 import json
-# Makes things more organised
-views = Blueprint('views', __name__)
+
+
+
+# Makes things more organised 
+views = Blueprint('views', __name__) 
+
 @views.route('/', methods = ['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-        if len(note) < 1:
-            flash('Please enter a note!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Your note has been saved!', category='success')
     return render_template("home.html", user=current_user)
+
+
 @views.route('/delete-note', methods = ['POST'])
 def delete_note():
     note = json.loads(request.data)
@@ -32,13 +31,29 @@ def delete_note():
             db.session.commit()
             flash('Your note has been deleted!', category='success')
         else:
-            flash('You do not have permission to delete this note!', category='error')
+            flash('You do not have permission to delete this note!', category='error')      
     return jsonify({})
-@views.route("/todolist")
+
+
+@views.route("/todolist", methods=['GET', 'POST'])
 @login_required
 def todolist():
     todo_list = Todo.query.all()
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Please enter a note!', category='error')
+            
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Your note has been saved!', category='success')
+    
     return render_template("todolist.html", todo_list=todo_list, user=current_user)
+
+
 @views.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
@@ -51,18 +66,23 @@ def add():
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("views.todolist"))
+
+
 @views.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("views.todolist"))
+
+
 @views.route("/delete/<int:todo_id>")
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("views.todolist"))
+
 @views.route("/paint")
 @login_required
 def paint():
